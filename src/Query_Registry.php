2b73f18c98777d1d883db51adc32aeb71094cbf7
Justin Vogt <mail@justin-vogt.de>
2024-08-14 14:19:03 +0200

    Update Query_Registry.php

--- src/Query_Registry.php
+++ src/Query_Registry.php
@@ -5,8 +5,21 @@ namespace juvo\Bricks_Custom_Queries;
 class Query_Registry
 {
 
+	/**
+	 * Query history through the lifetime of a request
+	 *
+	 * @var array
+	 */
+	private static array $history = [];
     private static ?Query_Registry $instance = null;
     private array $storage = [];
+
+    /**
+     * We store each query made with its unique id inside this array
+     *
+     * @var array
+     */
+    public static array $query_history = [];
 
     private function __construct() {
         add_action('pre_get_posts', function() {
@@ -61,6 +74,7 @@ class Query_Registry
             add_filter('bricks/query/loop_object', [$query, 'bricks_query_loop_object'], 10, 3);
             add_action('bricks/query/after_loop', [$query, 'bricks_query_after_loop'], 10, 2);
             add_filter('bricks/setup/control_options', [$query, 'bricks_add_query_type']);
+            add_filter('bricks/query/result_max_num_pages', [$query, 'bricks_query_result_max_num_pages'], 10, 2);
 
             // Register Loops for some elements
             $elements = ['container', 'block', 'div'];
@@ -70,5 +84,51 @@ class Query_Registry
         }
 
     }
+
+	/**
+	 * Generate a universal deterministic key for the query.
+	 *
+	 * @param string $element_id
+	 *
+	 * @return string
+	 */
+	private static function build_history_key(string $element_id): string {
+		return $element_id;
+	}
+
+	/**
+	 * Adds a query to the global history.
+	 *
+	 * @param \Bricks\Query $bricks_query
+	 * @param Query $query
+	 * @param array $results
+	 *
+	 * @return void
+	 */
+	public static function add_to_history(\Bricks\Query $bricks_query, Query $query, array $results = []): void {
+		self::$history[self::build_history_key($bricks_query->element_id)] = [
+			'results' => $results,
+			'max_pages' => $query->getMaxPages(),
+			'total' => $query->getTotal()
+		];
+	}
+
+	/**
+	 * Tries to receive a query from global history
+	 *
+	 * @param string $id
+	 *
+	 * @return false|array
+	 */
+	public static function get_from_history(string $element_id): false|array {
+
+		$key = self::build_history_key($element_id);
+
+		if (!in_array($key, array_keys(self::$history))) {
+			return false;
+		}
+
+		return self::$history[$key];
+	}
 
 }
