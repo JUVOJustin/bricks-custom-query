68952c82c226aff9a80bbda9b0917c58dd0a5163
Laura Herzog <laura.herzog@citation.media>
2024-08-28 08:58:13 +0200

    Docstrings

--- src/Language_Control.php
+++ src/Language_Control.php
@@ -1,18 +1,36 @@
 <?php
-/**
- * @license GPL-3.0-or-later
- *
- * Modified by Justin Vogt on 27-August-2024 using {@see https://github.com/BrianHenryIE/strauss}.
- */
-
 namespace juvo\Bricks_Custom_Queries;
 
+/**
+ * Class Language_Control
+ *
+ * This class is used to manage language-related controls and arguments for custom queries.
+ * It provides methods to add language arguments to query arrays and to generate control configurations.
+ */
 class Language_Control
 {
-
+    /**
+     * The name of the language control.
+     *
+     * @var string
+     */
     private string $name;
+
+    /**
+     * The label for the language control.
+     *
+     * @var string
+     */
     private string $label;
 
+    /**
+     * Language_Control constructor.
+     *
+     * Initializes the language control with a name and label.
+     *
+     * @param string $name  The name of the language control.
+     * @param string $label The label for the language control.
+     */
     public function __construct(string $name, string $label)
     {
         $this->name = $name;
@@ -20,27 +38,39 @@ class Language_Control
     }
 
     /**
-     * @param array $args
-     * @param $query_obj
-     * @param Query_Type $type
-     * @return array
+     * Adds a language argument to the query arguments array if a specific language setting is provided.
+     *
+     * @param array $args          The existing query arguments array.
+     * @param object $query_obj    The query object that may contain settings related to language.
+     * @param Query_Type $type     The query type object (or any other type expected for $type).
+     * @return array               The modified query arguments array with the added language argument.
      */
-    public function add_language_arg(array $args, $query_obj, Query_Type $type): array {
-
-        if (empty($query_obj->settings['language_'.$this->name])) {
+    public function add_language_arg(array $args, object $query_obj, Query_Type $type): array
+    {
+        if (empty($query_obj->settings['language_' . $this->name])) {
             return $args;
         }
-        
-        $args['language'] = $query_obj->settings['language_'.$this->name];
-        
+
+        $args['language'] = $query_obj->settings['language_' . $this->name];
+
         return $args;
     }
 
-    public function language_control()
+    /**
+     * Generates the control configuration for the language control.
+     *
+     * This method returns an array configuration for a language control,
+     * including a label and a type (in this case, 'text').
+     *
+     * @return array The control configuration array.
+     */
+    public function language_control(): array
     {
-        return ['language_' . $this->name => [
-            'label' => esc_html__( $this->label ),
-            'type'  => 'text'
-        ]];
+        return [
+            'language_' . $this->name => [
+                'label' => esc_html__($this->label),
+                'type'  => 'text',
+            ]
+        ];
     }
 }
